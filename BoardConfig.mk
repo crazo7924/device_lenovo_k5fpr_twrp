d512854ae28ed1f1095267ea089352df17ba12a5
SahilSonar <sss.sonar2003@gmail.com>
2020-03-15 13:09:40 +0000

    k5fpr: Switch to kernel source instead of prebuilt
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,8 @@ BOARD_KERNEL_CMDLINE   := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive 
 BOARD_KERNEL_BASE      := 0x40078000
 BOARD_KERNEL_PAGESIZE  := 2048
 BOARD_MKBOOTIMG_ARGS   := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
-TARGET_PREBUILT_KERNEL := device/lenovo/k5fpr/prebuilt/kernel
+TARGET_KERNEL_SOURCE := kernel/lenovo/k5fpr
+TARGET_KERNEL_CONFIG := k5fpr_defconfig
 
 # TARGET IMAGES
 TARGET_USERIMAGES_USE_EXT4 := true
